0fa9bfb38e8dfe2b284bcb6b9f722d654958b4da
ZeroX <zeroxt2002@gmail.com>
2023-07-17 13:43:10 +0700

    Full ex_1

--- mapreduce_exercises/ex_1/Ex_1.java
+++ mapreduce_exercises/ex_1/Ex_1.java
@@ -1,4 +1,4 @@
-package wordcount;
+package ex_1;
 
 import java.io.IOException;
 import java.util.StringTokenizer;
@@ -15,45 +15,57 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class Ex_1 {
 
-  public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+  public static class GraphMapper extends Mapper<Object, Text, Text, Text>{
 
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
-
+    private GraphSolver graph;
+    protected void setup(Context context) throws IOException, InterruptedException{
+      graph = new GraphSolver();
+    }
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
       String line = value.toString();
-      String[] vertice
-      while (itr.hasMoreTokens()) {
-        word.set(itr.nextToken());
-        context.write(word, one);
-      }
+      String[] vertice = line.split(" ");
+
+      String src = vertice[0];
+      String des = vertice[1];
+
+      graph.addVertex(src);
+      graph.addVertex(des);
+      graph.addEdge(src,des);
+
+
+    }
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+      for (String v: graph.getAllVertice()){
+          String type = graph.getType_Vertex(v);
+          context.write(new Text(v), new Text(type));
+        }
     }
   }
 
-  public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
-    private IntWritable result = new IntWritable();
+  public static class GraphReducer extends Reducer<Text,Text,Text,Text> {
+    private Text value = new Text();
 
-    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-      int sum = 0;
-      for (IntWritable val : values) {
-        sum += val.get();
+    public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+      for (Text type: values){
+        value.set(type);
+        break;
       }
-      result.set(sum);
-      context.write(key, result);
+      context.write(key,value);
     }
   }
 
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "WordCount");
-    job.setJarByClass(WordCount.class);
-    job.setMapperClass(TokenizerMapper.class);
-    job.setCombinerClass(IntSumReducer.class);
-    job.setReducerClass(IntSumReducer.class);
+    Job job = Job.getInstance(conf, "Ex_1");
+    job.setJarByClass(Ex_1.class);
+    job.setMapperClass(GraphMapper.class);
+    job.setCombinerClass(GraphReducer.class);
+    job.setReducerClass(GraphReducer.class);
     job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
+    job.setOutputValueClass(Text.class);
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
 }
+
---
+++ mapreduce_exercises/ex_1/GraphSolver.java
@@ -0,0 +1,71 @@
+package ex_1;
+
+import java.util.*;
+
+public class GraphSolver {
+  private Map<String,List<String>> Vector;
+  
+  public GraphSolver() {
+	  this.Vector = new HashMap<>();
+  }
+  
+  public void addVertex(String vertex) {
+	  Vector.putIfAbsent(vertex, new ArrayList<>());
+  }
+  
+  public void addEdge(String src,String des) {
+	  Vector.get(src).add(des);
+  }
+  
+  public Set<String> getAllVertice(){
+	  return Vector.keySet();
+  }
+  
+  public List<String> getAllDes(String src){
+	  return Vector.get(src);
+	  
+  }
+  
+  public String getType_Vertex(String vertex) {
+	  int count_src = 0;
+	  int count_des = 0;
+	  
+	  for (String v: this.getAllVertice()) {
+		  if (v.equals(vertex)) {
+			  count_src = this.getAllDes(v).size();
+		  }
+		  else {
+			  List<String> allDes = this.getAllDes(v);
+			  if (allDes.contains(vertex)) {
+				  count_des++;
+			  }
+		  }
+	  }
+	   if (count_des - count_src < 0) {
+          return "pos";
+      } else if (count_des - count_src > 0) {
+          return "neg";
+      } else {
+          return "eq";
+      }
+
+  }
+//   public static void main(String[] args) {
+// 	  GraphSolver graph = new GraphSolver();
+// 	  graph.addVertex("A");
+// 	  graph.addVertex("B");
+// 	  graph.addVertex("C");
+// 	  graph.addVertex("D");
+// 	  graph.addVertex("E");
+// 	  graph.addEdge("A", "B");
+// 	  graph.addEdge("B", "C");
+// 	  graph.addEdge("A", "C");
+// 	  graph.addEdge("D","E");
+	  
+// 	  for (String v: graph.getAllVertice()) {
+// 		  String type = graph.getType_Vertex(v);
+// 		  System.out.println(v + " " + type);
+// 	  }
+	  
+//   }
+}
