2bec3a59734e2849f5e6084283d309ce9d2f92c6
ZeroX <zeroxt2002@gmail.com>
2023-07-17 17:50:54 +0700

    Full ex_5

---
+++ mapreduce_exercises/ex_5/Ex_5.java
@@ -0,0 +1,59 @@
+package ex_5;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class Ex_5 {
+
+  public static class PatentMapper extends Mapper<Object, Text, Text, IntWritable>{
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        String[] patent_aspatent = line.split(" ");
+        String patent = patent_aspatent[0];
+        // String aspatent = patent_aspatent[1];
+        context.write(new Text(patent),one);
+    }
+  }
+
+  public static class PatentReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+
+    private IntWritable result = new IntWritable();
+    
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "Patent Program");
+    job.setJarByClass(Ex_5.class);
+    job.setMapperClass(PatentMapper.class);
+    job.setCombinerClass(PatentReducer.class);
+    job.setReducerClass(PatentReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
---
+++ mapreduce_exercises/ex_5/Ex_5_Paraphrase.md
@@ -0,0 +1,15 @@
+# Exercise 5: 
+All Ex 2 -> 11 are from 
+**Hadoop - MapReduce Lab - Map|Reduce|Driver - Sriram Balasubramanian**
+## Format input: 
+(patent) (associated_sub_patent)
+## Content: 
+Process a dataset with patent records. I will calculate the number of sub-patents associated with each patent
+## Example  
+**Sample dataset:** 
+![Sample Ex 5](/images/Sample_Ex_5.png)
+=> **Result**:        
+![Paraphrase Ex 5](/images/Paraphrase_Ex_5.png)
+
+In sample dataset is only have number but in input_ex_5.txt, I will use text data => It will describe this topic more fluently 
+
